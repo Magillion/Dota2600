a65c736472491cf7b845a16b40a0e0d4790899e2
chloe-goument <chloe2.goument@live.uwe.ac.uk>
2017-09-27 17:07:01 +0100

    Added basic enemy prefab, from this we can build creeps and towers

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public int playerteam;
+    public int enemyhealth = 5;
+    public int enemydamage = 2;
+    public float attackspeed = 1;
+    public float enemyrange = 1.5f;
+
+	// Use this for initialization
+	void Start ()
+    { 
+        GetComponent<CircleCollider2D>().radius = enemyrange;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        //Movement code should go here
+    }
+
+    // Whatever number is fed into this will be taken off
+    // of the health of this entity.
+    public void TakeDamage(int damage)
+    {
+        enemyhealth = enemyhealth - damage;
+    }
+
+    // To be called when applying damage to other entities.
+    public int DoDamage()
+    {
+        return enemydamage;
+    }
+}
