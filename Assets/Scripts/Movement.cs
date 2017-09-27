3251d97f544b8d6a9a7dd15e5c6554e97f041edb
Harry McAlpine <harry.mcalpine@gmail.com>
2017-09-27 17:22:21 +0100

    Input manager setup
    Two players (single prefab created called player)
    All generic controller types supported
    Character movement in X & Y setup for player 1 and 2
    Bounding added to keep players on screen

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+/*
+ * Moves the Player in X and Y
+ */
+
+[RequireComponent(typeof(Rigidbody))] //Throws error if no rigidBody is attached
+public class Movement : MonoBehaviour
+{
+    //Public variables
+    public float speed = 10f;
+    public string playerNumber;
+
+    //Private variables
+    private new Rigidbody2D rigidbody2D;
+    private Vector2 axisMovement;
+    private Vector3 newPos;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    } //End of start
+
+    private void FixedUpdate() //Physics2d attached
+    {
+        
+        float horizontal = Input.GetAxis("Horizontal " + playerNumber) * speed * Time.deltaTime; //Name the axis and player number 1,2,3,4 etc to allocate player to controller or player won't move
+        float vertical = Input.GetAxis("Vertical " + playerNumber) * speed * Time.deltaTime;
+
+        axisMovement = new Vector2(horizontal, vertical);
+
+        newPos = rigidbody2D.position + axisMovement;
+
+        rigidbody2D.MovePosition(newPos);
+       
+    } //End of FixedUpdate.
+}
---
+++ Assets/Scripts/boundToScreen.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+/*
+ * Binds the player to the screen
+ */
+
+public class boundToScreen : MonoBehaviour {
+
+    void LateUpdate() //Called after all the other update functions
+    {
+        var left = Camera.main.ViewportToWorldPoint(Vector3.zero).x; //Left bound of main camera
+        var right = Camera.main.ViewportToWorldPoint(Vector3.one).x; //Right bound of main camera
+        var top = Camera.main.ViewportToWorldPoint(Vector3.zero).y; //Top bound of main camera
+        var bottom = Camera.main.ViewportToWorldPoint(Vector3.one).y; //Bottom bound of main camera
+
+        float x = transform.position.x, y = transform.position.y; // Current Player position
+
+        //Left and right checks against main camera bounds
+        if (transform.position.x < left + GetComponent<Renderer>().bounds.extents.x)
+        {
+            x = left + GetComponent<Renderer>().bounds.extents.x;
+        }
+        else if (transform.position.x > right - GetComponent<Renderer>().bounds.extents.x)
+        {
+            x = right - GetComponent<Renderer>().bounds.extents.x;
+        }
+
+        //Top and bottom checks against main camera bounds
+        if (transform.position.y <= top + GetComponent<Renderer>().bounds.extents.y)
+        {
+            y = top + GetComponent<Renderer>().bounds.extents.y;
+        }
+        else if (transform.position.y >= bottom - GetComponent<Renderer>().bounds.extents.y)
+        {
+            y = bottom - GetComponent<Renderer>().bounds.extents.y;
+        }
+        transform.position = new Vector3(x, y, transform.position.z);
+    }//End of lateupdate
+}
