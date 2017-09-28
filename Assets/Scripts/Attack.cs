f8484e220c8ccb51e654c138b8ab41aa30e9f259
Magillion <Max2.Magill@liive.uwe.ac>
2017-09-28 20:42:36 +0100

    Added an attack script which is related to the trigger of the object it is on. It creates a list of all the objects inside the trigger, adding objects when they enter and remove them when they leave. Currently doing no damage because of no proper health system so next step is to implement the ability to cycle through the list attacking them. Possible to get the closest object and attack that.

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+
+    List<Collider2D> enemiesInRangeList = new List<Collider2D>();//list of enemies in range
+    GameObject enemyGameObject; //gameobject of the enemy
+    Tower tower;
+
+    public int enemyDamage = 2;
+    public float attackSpeed = 1;
+
+
+    void Start()
+    {
+        tower = GetComponent<Tower>(); //Currently getting a reference for the tower because of the way project is set up (will become a health reference)
+    }
+
+/// <summary>
+/// When a collider enters the trigger, check it's an enemy and then add to a list of enemies 
+/// so they can be attacked one by one. enemyInRange is the object that has entered the trigger
+/// </summary>
+/// <param name="enemyInRange"></param>
+    void OnTriggerEnter(Collider2D enemyInRange)
+    {
+        enemyGameObject = enemyInRange.transform.gameObject;
+        Movement enemyMovment = enemyInRange.GetComponent<Movement>();
+
+        if (int.Parse(enemyMovment.playerNumber) != tower.playerTeam)
+        {
+            if (!enemiesInRangeList.Contains(enemyInRange))
+            {
+                enemiesInRangeList.Add(enemyInRange);
+            }
+        }
+
+        foreach (Collider2D enem in enemiesInRangeList)
+        {
+            GameObject enemyToHit = enem.transform.gameObject;
+            //TODO: when players/creeps have health reduce it here with set damage
+        }
+    }
+    /// <summary>
+    /// Remove the enemy from the list of enemies
+    /// </summary>
+    /// <param name="enemyLeavingRange"></param>
+    void OnTriggerExit(Collider2D enemyLeavingRange)
+    {
+        if (enemiesInRangeList.Contains(enemyLeavingRange))
+        {
+            enemiesInRangeList.Remove(enemyLeavingRange);
+        }
+    }
+
+    // To be called when applying damage to other entities.
+    public int DoDamage()
+    {
+        return enemyDamage;
+    }
+}
+
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,14 +1,14 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
 
 public class Tower : MonoBehaviour
 {
-    public int playerteam;
-    public int enemyhealth = 20;
-    public int currenthealth = 20;
-    public int enemydamage = 2;
-    public float attackspeed = 1;
-    public float enemyrange = 1.5f;
+    public int playerTeam;
+    public int enemyHealth = 20;
+    public int currentHealth = 20;
+    public float enemyRange = 1.5f;
+
 
     // Use this for initialization
     void Start()
@@ -20,11 +20,11 @@ public class Tower : MonoBehaviour
     void Update()
     {
         // Simple change colour based on health level
-        if (currenthealth < enemyhealth / 4)
+        if (currentHealth < enemyHealth / 4)
         {
             GetComponent<SpriteRenderer>().color = Color.red;
         }
-        else if (currenthealth < enemyhealth / 2)
+        else if (currentHealth < enemyHealth / 2)
         {
 
             GetComponent<SpriteRenderer>().color = Color.yellow;
@@ -35,12 +35,9 @@ public class Tower : MonoBehaviour
     // of the health of this entity.
     public void TakeDamage(int damage)
     {
-        currenthealth = currenthealth - damage;
+        currentHealth = currentHealth - damage;
     }
 
-    // To be called when applying damage to other entities.
-    public int DoDamage()
-    {
-        return enemydamage;
-    }
+
 }
+   
